18a29dde962f7df09d030654a0781ad6fee95c0d
GhilasIC <kahloucheg@gmail.com>
2025-10-19 14:06:44 +0200

    Update project.vhdl

--- src/project.vhdl
+++ src/project.vhdl
@@ -2,24 +2,44 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity tt_um_example is
+entity tt_um_vhdl_fsm is
     port (
         ui_in   : in  std_logic_vector(7 downto 0);
         uo_out  : out std_logic_vector(7 downto 0);
         uio_in  : in  std_logic_vector(7 downto 0);
         uio_out : out std_logic_vector(7 downto 0);
         uio_oe  : out std_logic_vector(7 downto 0);
-        ena     : in  std_logic;
+        ena     : in  std_logic;  -- unused
         clk     : in  std_logic;
         rst_n   : in  std_logic
     );
-end tt_um_example;
+end tt_um_vhdl_fsm;
 
-architecture Behavioral of tt_um_example is
+architecture rtl of tt_um_vhdl_fsm is
+    signal reset_s    : std_logic;  -- active-high internal reset
+    signal dispense_s : std_logic;
+    signal change_s   : std_logic;
 begin
+    -- Convert active-low board reset to active-high internal reset
+    reset_s <= not rst_n;
 
-    uo_out <= std_logic_vector(unsigned(ui_in) + unsigned(uio_in));
-    uio_out <= "00000000";
-    uio_oe <= "00000000";
+    -- Keep uio pins as inputs (don’t drive them)
+    uio_out <= (others => '0');
+    uio_oe  <= (others => '0');
 
-end Behavioral;
+    -- Map outputs: bit0=dispense, bit1=change, others low
+    uo_out(0) <= dispense_s;
+    uo_out(1) <= change_s;
+    uo_out(7 downto 2) <= (others => '0');
+
+    -- FSM instance
+    u_vm : entity work.vending_machine
+        port map (
+            clk      => clk,
+            reset    => reset_s,          -- active-high
+            coin     => ui_in(0),         -- coin input
+            btn      => ui_in(2 downto 1),-- 2-bit buttons
+            dispense => dispense_s,
+            change   => change_s
+        );
+end rtl;
